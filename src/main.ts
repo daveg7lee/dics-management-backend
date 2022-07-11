c383c48a1db1ddb69b65a1bd295f585a48d7d3bd
Dave <daveg7lee@gmail.com>
2022-07-11 20:30:10 +0900

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
@@ -5,28 +6,14 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
-const CORS_OPTIONS = {
-  origin: ['*'],
-  allowedHeaders: [
-    'Access-Control-Allow-Origin',
-    'Origin',
-    'X-Requested-With',
-    'Accept',
-    'Content-Type',
-    'x-jwt',
-  ],
-  exposedHeaders: 'x-jwt',
-  methods: ['GET', 'PUT', 'OPTIONS', 'POST', 'DELETE'],
-};
-
 async function bootstrap() {
-  const adapter = new FastifyAdapter();
-  adapter.enableCors(CORS_OPTIONS);
-
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors()
 
   await app.listen(4000, '0.0.0.0');
 }
