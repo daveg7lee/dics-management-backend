51bb9788171765021f67781c332fd6170f06685c
Dave <daveg7lee@gmail.com>
2022-07-20 20:12:57 +0900

    send email when score upload

--- src/scores/scores.service.ts
+++ src/scores/scores.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import * as nodeoutlook from 'nodejs-nodemailer-outlook';
 import prisma from '../prisma';
 import { CreateScoreInput, ScoreOutput } from './dto/create-score.input';
 import { ScoresOutput } from './dto/scores.dto';
@@ -15,6 +16,12 @@ export class ScoresService {
     username,
   }: CreateScoreInput): Promise<ScoreOutput> {
     try {
+      const userExists = await prisma.user.findUnique({ where: { username } });
+
+      if (!userExists) {
+        throw new Error('존재하지 않는 학생입니다.');
+      }
+
       const createdScore = await prisma.score.create({
         data: {
           score,
@@ -26,6 +33,21 @@ export class ScoresService {
           detail,
         },
       });
+
+      if (userExists.email && type === 'Demerit') {
+        await nodeoutlook.sendEmail({
+          auth: {
+            user: process.env.MAIL_ADDRESS,
+            pass: process.env.MAIL_PASSWORD,
+          },
+          from: process.env.MAIL_ADDRESS,
+          to: userExists.email,
+          subject: `벌점 ${score}점이 입력되었습니다.`,
+          html: `<h1>다음과 같은 사유로 벌점 ${score}점이 입력되었습니다.</h1> <h2>${article}</h2>`,
+          text: article,
+          replyTo: 'dicscouncil@gmail.com',
+        });
+      }
 
       return { success: true, score: createdScore };
     } catch (e) {
